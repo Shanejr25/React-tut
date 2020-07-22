5bd507054cd1186e4a44028f904ea1fba60b53e5
unknown <stlazjr0@gmail.com>
2020-07-22 03:00:39 -0700

    started messing around

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  // define methods that will be used
+  onClick() {
+    alert('Clicked');
+  }
+  onMouseEnter() {
+    alert('Entered');
+  }
+  
+  onChange(event) { // event
+    console.log(event.target.value);
+  }
+
+  render() { // loads the things onto the page
+    
+    const list = [ // array of strings
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Random'
+    ];
+
+    const title ='This is Shanes React App';
+    const anotherTitle = 'Another title';
+
+    return (
+      <div className ="App">
+        <h1>
+        {
+          true ? anotherTitle : title
+          
+        }
+      </h1>
+      <h2>
+        { // must use brackets to encase the contents of the headers
+          list.map(item => {
+              return (
+                <div>{item}</div>
+              );
+            })
+        }
+      </h2>
+      <h1>
+        {
+          list.map(item => {
+            return (
+              <div onClick={this.onClick}>{item}</div>
+            );
+          })
+        }
+      </h1>
+      <h3>
+        {
+          list.map(item => {
+            return (
+              <div onMouseEnter={this.onMouseEnter}>{item}</div>
+            );
+          })
+        }
+      </h3>
+      <h4>
+        {
+          list.map(item => {
+            return (
+              <div key={item} onClick={this.onClick}>{item}</div>
+            );
+          })
+        }
+      </h4>
+      <input onChange={this.onChange} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
